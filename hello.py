72623f1760df07e3983ca8f6e63e7fd3722a31fb
AmAmit <amitkumar08141996@gmail.com>
2016-10-04 16:10:42 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,3 @@
+print("Hello World")
+print("What is your name?")
+print("Excited to be onboard")
---
+++ sum n.py
@@ -0,0 +1,7 @@
+n=int(input("Enter the value of n till which you want your sum : "))
+i=0
+summ=0
+while i<=n:
+    summ+=i
+    i+=1
+print("Total sum is ",summ)
